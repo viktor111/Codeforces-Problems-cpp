353d8ec177ff56ed8d8d53cbdefa2e5e608dc097
Viktor Draganov <swifrorlilko@gmail.com>
2020-09-23 22:25:26 +0300

    add pytaandstrings

---
+++ DominoPiling/domino.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<iostream>
+
+using std::cout;
+using std::cin;
+
+int main(){
+    int m, n;
+
+    cin >> m >> n;
+
+    cout << m * n / 2;
+}
+    
---
+++ PetyaAndStrings/petyaandstrings.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main(){
+
+}
+    
